404047b779a7da58769986c076d4abaabb950cc0
omar <ohafiz97@gmail.com>
2015-05-11 20:46:12 -0500

    here it is

---
+++ RailFence.java
@@ -0,0 +1,81 @@
+
+/**
+ * Write a description of class RailFence here.
+ * 
+ * @author (your name) 
+ * @version (a version number or a date)
+ */
+public class RailFence
+{
+
+
+    /**
+     * Constructor for objects of class RailFence
+     */
+    public RailFence()
+    {
+        
+    }
+    
+    public String encode(String s)throws Exception
+ {
+  int r=3;
+  int c=s.length()/3;
+  char mat[][]=new char[r][c];
+  int k=0;
+   
+  String cipherText="";
+   
+  for(int i=0;i< c;i++)
+  {
+   for(int j=0;j< r;j++)
+   {
+    if(k!=s.length())
+     mat[j][i]=s.charAt(k++);
+    else
+     mat[j][i]='X';
+   }
+  }
+  for(int i=0;i< r;i++)
+  {
+   for(int j=0;j< c;j++)
+   {
+    cipherText+=mat[i][j];
+   }
+  }
+  return cipherText;
+ }
+    
+   public String decode(String s)throws Exception
+ {
+  int r=3;
+  int c=s.length()/3;
+  char mat[][]=new char[r][c];
+  int k=0;
+  while (s.length()/3*3 != s.length())
+  {
+      s = s + " ";
+    }
+  
+  String plainText="";
+   
+   
+  for(int i=0;i< r;i++)
+  {
+   for(int j=0;j< c;j++)
+   {
+    mat[i][j]=s.charAt(k++);
+   }
+  }
+  for(int i=0;i< c;i++)
+  {
+   for(int j=0;j< r;j++)
+   {
+    plainText+=mat[j][i];
+   }
+  }
+   
+  return plainText;
+ }
+}
+
